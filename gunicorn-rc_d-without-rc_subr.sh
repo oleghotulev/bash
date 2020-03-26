7ec9c99659a040a331b2ac5db443b0d0912aeaa6
Oleg <41948080+oleghotulev@users.noreply.github.com>
2020-03-26 22:26:30 +0200

    Update gunicorn-rc_d-without-rc_subr.sh

--- gunicorn-rc_d-without-rc_subr.sh
+++ gunicorn-rc_d-without-rc_subr.sh
@@ -1,1 +1,46 @@
+#!/bin/sh
 
+GUNICORN=/usr/local/bin/gunicorn
+ROOT=/var/www/mysite
+PID=/var/run/gunicorn.pid
+APP=mysite.wsgi:application
+
+stop_app () {
+    if [ -f $PID ]; then
+        kill $(cat $PID)
+    fi
+}
+
+start_app () {
+    cd $ROOT && $GUNICORN -c $ROOT/gunicorn.conf.py --pid=$PID $APP --daemon
+}
+
+status_app () {
+    if [ -f $PID ]; then
+        echo "Gunicorn is runnig !!!"
+    else
+        echo "Gunicorn NOT run !!!"
+    fi
+}
+
+case $1 in
+    start)
+        start_app
+        ;;
+    restart)
+        stop_app && start_app
+        ;;
+    stop)
+        stop_app
+        ;;
+    status)
+        status_app
+        ;;
+    *)
+        if [ -f $PID ]; then
+            echo "Type start, stop, restart"
+        else
+            cd $ROOT && $GUNICORN -c $ROOT/gunicorn.conf.py --pid=$PID $APP --daemon
+        fi
+        ;;
+esac
